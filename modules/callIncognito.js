d2ab8a9d5feec26be4b54ae0354a7a78d859a159
SRCthird <SRCthird@gmail.com>
2023-08-28 17:39:26 -0400

    Created the incognito proxy

---
+++ modules/callIncognito.js
@@ -0,0 +1,16 @@
+import { exec } from 'child_process';
+
+const callIncognito = (url) => {
+    const command = `start msedge -inprivate "${url}"`
+    
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+}
+
+export default callIncognito;
